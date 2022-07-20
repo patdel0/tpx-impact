14b235767d21b615223119e9da61c1ed731b69f2
DogwishX <zefiru@protonmail.com>
2022-07-20 09:01:33 +0100

    Add naming conventions and constant creation to Learnings

--- text-based-game/index.php
+++ text-based-game/index.php
@@ -1,5 +1,8 @@
 
 <?php
-$obj = (object) ['name' => 'patrick'];
-echo $obj->name
+define('ROOM1', (object)['name' => 'Room1', 'flavoured-text' => 'You are in a dungeon. A goblin stares at you menacingly']);
+define('ROOMS', array($ROOM1));
+
+echo ROOM1;
+
 ?>
